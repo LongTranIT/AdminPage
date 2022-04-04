9ec987f898261d66821aca371484c72fb9566448
LongTranIT <tranphilongnvd@gmail.com>
2022-04-04 10:51:31 +0700

    add delete function js

--- src/public/js/myScript.js
+++ src/public/js/myScript.js
@@ -16,3 +16,30 @@ function setNavigation() {
         }
     });
 };
+
+//Handle Delete
+document.addEventListener('DOMContentLoaded', function () {
+    var Id;
+    var btnDelete = document.querySelector('#btn-delete');
+    var deleteForm = document.forms['delete-form'];
+
+    $('.modal-delete').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        Id = button.data('id'); // Extract info from data-* attributes
+        console.log(Id)
+        var name = button.data('name');
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this);
+        modal.find('.modal-body').text('Bạn có chắc chắn muốn xóa ' + name+' không?');
+
+    });
+
+    //Handle click delete button
+    btnDelete.onclick = () => {
+        deleteForm.action = location.href+'/' + Id + '?_method=DELETE';
+        deleteForm.submit();
+    }
+    
+    
+});
